00636ae5cae5680ab1d4c272929373dc6aaff45d
Ganesan-Elroi <ganesan.j@erssmail.com>
2026-06-25 12:57:56 +0530

    v1

---
+++ .github/workflows/readme.md
@@ -0,0 +1,2 @@
+## https://github.com/cloudfoundry/cli/releases?utm_source=chatgpt.com
+# app url: https://ganesan-flask-hello-768439a0.cfapps.ap21.hana.ondemand.com/
---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World! Flask app deployed on SAP BTP Cloud Foundry."
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "UP",
+        "message": "Flask application is healthy"
+    }), 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
